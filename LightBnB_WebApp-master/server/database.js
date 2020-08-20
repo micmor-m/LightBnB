2be8769361e40ffddc73f1c6dd1789784c4be02b
micmor-m <micmor.m@gmail.com>
2020-08-20 21:04:18 +0000

    add RETURNING *; to addUser function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -74,7 +74,8 @@ return pool.query(`
 INSERT INTO users (
 name, email, password) 
 VALUES (
-$1, $2, $3);
+$1, $2, $3)
+RETURNING *;
 `, [user.name, user.email, user.password])
 .then(res => res.rows)
 .catch(() => NULL)
